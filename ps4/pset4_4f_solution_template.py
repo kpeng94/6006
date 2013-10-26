229518e03a17e020785bb960cdfd9584ed0f8b00
Kevin Peng <kpeng94@mit.edu>
2013-10-26 04:46:07 -0400

    Added ps4 skel

---
+++ ps4/pset4_4f_solution_template.py
@@ -0,0 +1,30 @@
+# This solution template should be turned in through our submission site, at
+# https://alg.csail.mit.edu
+
+######################################################################################
+### WARNING:                                                                       ###
+### Be sure to write the Python code yourself!  We do run sophisticated automated  ###
+### comparisons between each pair of programs turned in.  We are saddened and      ###
+### troubled each year when a few studentsturn in nearly identical programs, and   ###
+### we have to administer appropriate consequences.  It is better to turn in a     ###
+### statement that you didn't have time to complete the assignment than to turn in ###
+### the same code as someone else.                                                 ###
+######################################################################################
+
+
+####################
+### Problem 4-4f ###
+####################
+
+######################################################################################
+## N will be an integer, e.g. 4                                                     ##
+## B will be a tuple of tuples rep'ing boulder locations, e.g. ((1,1),(2,2))        ##
+## C will be a tuple of tuples rep'ing car locations, e.g. ((0,0),(0,3),(2,1),(3,2))##
+## Returned result should be a list of tuples of tuples each rep'ing a move         ##
+## e.g. [((0,3), (0,2)), ((3,2), (3,1)), ((3,1), (2,1)), ((0,2), (0,1)),            ##
+##       ((0,1), (0,0)), ((0,0), (1,0)), ((1,0), (2,0)), ((2,0), (2,1))]            ##
+######################################################################################
+
+### Implement me! ###
+def DemoDerby(N, B, C):
+    pass
