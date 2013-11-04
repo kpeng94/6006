7a6f1563840b409cc590a54641c02ab195c8bb77
Kevin Peng <kpeng94@mit.edu>
2013-11-04 07:45:38 -0500

    Problem 1 done, but working on doing modifications to .py file

--- ps4/pset4_4f_solution_template.py
+++ ps4/pset4_4f_solution_template.py
@@ -25,6 +25,94 @@
 ##       ((0,1), (0,0)), ((0,0), (1,0)), ((1,0), (2,0)), ((2,0), (2,1))]            ##
 ######################################################################################
 
+class DemoDerbyState:
+  def __init__(self, car_locations, moves_set, move, parent):
+    self.car_locations = car_locations
+    self.moves_set = moves_set
+    self.move = move
+    self.parent = parent
+
 ### Implement me! ###
 def DemoDerby(N, B, C):
-    pass
+  possible_locations_to_go_to = set()
+  # Map all points to a N + 2 by N + 2 grid
+  # The padding is for the movement later
+  # We add (1,1) so that the grid is centered properly
+  for i in xrange(N):
+    for j in xrange(N):
+      possible_locations_to_go_to.add((N + 2) * (i + 1) + (j + 1))
+  for boulder in B:
+    boulder_loc = (N + 2) * (boulder[0] + 1) + (boulder[1] + 1)
+    possible_locations_to_go_to.remove(boulder_loc)
+  states = []
+  moves_set = set()
+  visited_car_locations = set()
+  # State is a board + moves made so far
+  states.append(DemoDerbyState(set(C), moves_set, None, None))
+  while len(states) > 0:
+    currentState = states.pop(0)
+    current_car_locations = currentState.car_locations
+    current_moves_set = currentState.moves_set
+    for car_location in current_car_locations:
+      # Get the new car locations for each car_location
+      new_car_location_tuples = [N * (car_location[0] - 1) + car_location[1],
+                                 N * (car_location[0] + 1) + car_location[1],
+                                 N * (car_location[0]) + car_location[1] + 1,
+                                 (car_location[0], car_location[1] - 1)]
+      for new_car_location in new_car_location_tuples:
+        # Create a new move
+        move = (car_location, new_car_location)
+        # DP for checking whether this is already in the current_move_set
+        # and whether it is valid
+        if move not in current_moves_set and new_car_location in possible_locations_to_go_to:
+          # modify new car locations to contain the right locations for the
+          # cars
+          new_car_locations = current_car_locations.copy()
+          new_car_locations.remove(car_location)
+          new_car_locations.add(new_car_location)
+          if new_car_locations not in visited_car_locations:
+            new_moves_set = current_moves_set.copy()
+            new_moves_set.add(move)
+            if (len(new_car_locations) == 1):
+              moves_list = []
+              moves_list.append(move)
+              state = currentState
+              while state.move != None:
+                moves_list.append(state.move)
+                state = state.parent
+              moves_list.reverse()
+              return moves_list
+            states.append(DemoDerbyState(new_car_locations, new_moves_set, move, currentState))
+            visited_car_locations.add(new_car_locations)
+  return None
+
+
+N=2
+B=()
+C=((0, 0), (1, 1))
+print DemoDerby(N, B, C)
+
+N=3
+B=((1, 1), (1, 0))
+C=((0, 0), (0, 2), (2, 0), (2, 2))
+print DemoDerby(N, B, C)
+
+N=4
+B=((1, 1), (2, 2))
+C=((0, 0), (0, 3), (2, 1))
+print DemoDerby(N, B, C)
+
+N=4
+B=()
+C=((0, 0), (0, 3), (1, 2))
+print DemoDerby(N, B, C)
+
+N=4
+B=((1, 1), (2, 2))
+C=((0, 0), (0, 3), (2, 1), (3, 2))
+print DemoDerby(N, B, C)
+
+# N=5
+# B=((1, 1), (2, 2), (3, 3))
+# C=((0, 0), (4, 4), (1, 2), (3, 2))
+# print DemoDerby(N, B, C)
